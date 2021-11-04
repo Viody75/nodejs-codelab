45c53c704da6b3f7aa05aad59e530e9828d0b022
Viody Alfaridzi <viody75@gmail.com>
2021-11-04 17:35:35 +0800

    fix queries

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node.js Simple CRUD with Express.js and MySQL Tutorial' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Viody - Node.js CodeLab' });
 });
 
 module.exports = router;
